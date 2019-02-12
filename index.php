5ca258e5c564040bad841c3046cfa4ea632bc558
fisher <tiebayu@126.com>
2019-02-13 07:43:45 +0800

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @authors fisher (i@qnmlgb.trade)
+ * @date    2019-02-13
+ * @version 1.0
+ */
+
+require './config.php';
+require 'lib/publicBackstage.class.php';
+$pb = new publicBackstage($config);
+
+$mod = isset($_GET['mod']) ? $_GET['mod'] : 'index';
+$pb->urlRoute($mod);
