dc41e0324495172e30ad09afd5c37f37915b0776
fisher <tiebayu@126.com>
2019-10-13 14:18:24 +0800

    updata gitignore

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @authors fisher (i@qnmlgb.trade)
+ * @date    2019-02-13
+ * @version 1.0
+ */
+$config = [
+	'bduss' => "", //此处输入有后台查看权限吧务的BDUSS
+	'kw' => '', //在吧名处填写需要公开吧务后台的贴吧
+	'showpic' => true, //boolean值，是否开启图片（需要消耗服务器资源进行转存），可关闭
+	'hideopt' => true, //boolean值，是否隐藏操作人按钮，建议开启（保护操作人隐私）
+];
